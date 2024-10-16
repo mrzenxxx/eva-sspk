bb0012e2554353a56231cbfdb38410b8e1211b11
Archi Gene <44780361+mrzenxxx@users.noreply.github.com>
2024-10-16 21:41:35 +0300

    add: ThemeProvider

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store } from './store'
+import { ThemeProvider } from './components/context/theme-provider'
 import App from './App'
 import './global.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ThemeProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ThemeProvider>
   </StrictMode>
 )
